0df1dee802e41e9c216bc42d2fb0287f6d701d82
Yeonwha <ics22-177@camosun.bc.ca>
2025-04-24 11:19:46 -0700

    initial commit for  middleware and server

---
+++ express.js
@@ -0,0 +1,24 @@
+// Middleware from mode_modules/
+import express from 'express';   // express
+import cookieParser from 'cookie-parser';  // express uses to parse HTTP cookies in Request
+import compression from 'compression';     // server can accept compressed file
+import morgan from 'morgan';     // data logger to log responese and request in the console
+import cors from 'cors';         // enables CORS for API from different domain, protocol, port access API
+
+const app = express();
+
+// Express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// Request chain
+app.use(cookieParser());
+app.use(compression());
+app.use(morgan('dev'));
+app.use(cors());
+
+// Routing
+app.get('/', (req, res) => {
+    res.send('Node.js Server is live!');
+});
+
+export default app;
